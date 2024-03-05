115d1827dd2ba2f6c2879d205686e39723b7c7cb
shee-tal-b <107496785+shee-tal-b@users.noreply.github.com>
2024-03-05 20:40:49 +0530

    Second commit from Jenkins-main

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "SRN is PES2UG21CS493"
+  cout << "SRN is PES2UG21CS493";
   return 0;
 }
